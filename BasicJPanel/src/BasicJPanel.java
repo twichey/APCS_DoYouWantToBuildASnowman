c666d3f42e1d3e76d5791db8d37084af357fd036
twichey <bhawley321@gmail.com>
2014-09-02 11:33:41 -0400

    stuff

--- BasicJPanel/src/BasicJPanel.java
+++ BasicJPanel/src/BasicJPanel.java
@@ -28,8 +28,8 @@ public class BasicJPanel extends JPanel{
       made visible or moved or whatever.
     */
     public void paintComponent(Graphics g){
-        g.drawLine(10,10,450,450); // Draw a line from (10,10) to (150,150)
-        g.drawOval(200, 200, 50, 50);
+        g.drawOval(200, 200, 200, 200);
+        g.drawOval(200, 300, 200, 200);
     }
 
     public static void main(String arg[]){
